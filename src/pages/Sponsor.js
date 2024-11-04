9d167543d4b6acb0126de0a639b5717cf489099a
Vimalesh C T <pranovvimal30@gmail.com>
2024-11-04 23:00:14 +0530

    Sponsors

--- src/pages/Sponsor.js
+++ src/pages/Sponsor.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styles from "../styles/Sponsor.module.scss";
 import videoBg from "../assets/autumn-tree-cloudy-sky-sunset-moewalls-com.mp4";
+import kraftonLogo from "../assets/sponsor/krafton.jpg"; // Assuming the path to the uploaded Krafton logo
+import rajamsLogo from "../assets/sponsor/rajams.png"; // Assuming the path to the uploaded Rajams logo
 
 const Sponsor = () => (
   <div className={styles.container}>
@@ -20,11 +22,25 @@ const Sponsor = () => (
 
     <h1 className={styles["coming-soon"]}>Our Sponsors</h1>
     <p className={styles.message}>
-      We're gathering an incredible lineup of sponsors for you. Stay tuned!
+      We're proud to present our incredible lineup of sponsors!
     </p>
-    <div className={styles["animated-coming-soon"]}>
-      <span className={styles.coming}>Coming</span>
-      <span className={styles.soon}>Soon</span>
+
+    {/* Sponsors Section */}
+    <div className={styles.sponsors}>
+      <div className={styles.sponsorCard}>
+        <img src={kraftonLogo} alt="Krafton Logo" className={styles.logo} />
+        <div className={styles.sponsorInfo}>
+          <h2>Signature Events Sponsor</h2>
+          <p>Krafton</p>
+        </div>
+      </div>
+      <div className={styles.sponsorCard}>
+        <img src={rajamsLogo} alt="Rajams Logo" className={styles.logo} />
+        <div className={styles.sponsorInfo}>
+          <h2>Printing Partner</h2>
+          <p>Rajams</p>
+        </div>
+      </div>
     </div>
   </div>
 );
